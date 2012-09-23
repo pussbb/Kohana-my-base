1690d70c3021fc17541742df88ef02d3e53d6163
pussbb <pussbb@gmail.com>
2012-09-23 17:19:00 +0300

    eightpack tools solution
    Signed-off-by: pussbb <pussbb@gmail.com>

--- classes/tools/media.php
+++ classes/tools/media.php
@@ -1,6 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 /**
+ * Execute some external usefull tools (beautify and minize)
  * http://ariya.ofilabs.com/2011/10/javascript-tools-for-continuous-integration.html
  * @package Kohana-my-base
  * @copyright 2012 pussbb@gmail.com
@@ -14,28 +15,41 @@
 class Tools_Media extends Tools {
 
 
-/*
-$files = Dir::files(DOCROOT.'media/js', 'js');
-foreach($files as $file) {
-    Tools_Media::jsmin($file, 'min');
-}
-
-*/
-    protected function jsmin($file, $prefix = NULL)
+  /**
+  * minize js(files with .min.js will be skiped)
+  *
+  * <code>
+  *  $files = Dir::files(DOCROOT.'media/js', 'js');
+  *   foreach($files as $file) {
+  *     Tools_Media::jsmin($file, 'min');
+  *   }
+  * </code>
+  * @param $file string file name
+  * @param $prefix string prefix for minimized file if null rewrites origin
+  * @return void
+  */
+    protected function jsmin($file, $prefix = 'min')
     {
         if ($prefix && preg_match('/\.'.$prefix.'\.js/', $file))
             return;
         $dest_file = dirname($file).DIRECTORY_SEPARATOR.basename($file, '.js').".$prefix.js";
         $this->eightpack_exec('jsmin', $file, $dest_file);
     }
-/*
-$files = Dir::files(DOCROOT.'media/css', 'css');
-foreach($files as $file) {
-    Tools_Media::cssmin($file, 'min');
-}
 
-*/
-    protected function cssmin($file, $prefix = NULL)
+  /**
+  * minize css (files with .min.js will be skiped)
+  *
+  * <code>
+  *  $files = Dir::files(DOCROOT.'media/css', 'css');
+  *   foreach($files as $file) {
+  *     Tools_Media::cssmin($file, 'min');
+  *   }
+  * </code>
+  * @param $file string file name
+  * @param $prefix string prefix for minimized file if null rewrites origin
+  * @return void
+  */
+    protected function cssmin($file, $prefix = 'min')
     {
         if ($prefix && preg_match('/\.'.$prefix.'\.js/', $file))
             return;
@@ -43,6 +57,36 @@ foreach($files as $file) {
         $this->eightpack_exec('cssmin', $file, $dest_file);
     }
 
+
+    /**
+    * minize media files css and js (files with .min.js will be skiped)
+    *
+    * <code>
+    *  $files = Dir::files(DOCROOT.'media');
+    *   foreach($files as $file) {
+    *     Tools_Media::minimize($file, 'min');
+    *   }
+    * </code>
+    * @param $file string file name
+    * @param $prefix string prefix for minimized file if null rewrites origin
+    * @return void
+    */
+    protected function minimize($file, $prefix = 'min')
+    {
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
+        if ($extension == 'css')
+          return $this->cssmin($file, $prefix);
+        if ($extension == 'js')
+          return $this->jsmin($file, $prefix);
+    }
+
+    /**
+    * execute console application and write changes to the file
+    *
+    * @param $file string file name
+    * @param $prefix string prefix for minimized file if null rewrites origin
+    * @return void
+    */
     private function eightpack_exec($app, $source_file, $dest_file = NULL)
     {
       $ok = $this->exec(self::config("eightpack.$app")." $source_file");
@@ -52,19 +96,68 @@ foreach($files as $file) {
       file_put_contents($file, $this->stdout);
     }
 
-    protected function css_beautify($file, $prefix = NULL)
+    /**
+    * beautify media files css and js (files with .min.js will be skiped)
+    *
+    * <code>
+    *  $files = Dir::files(DOCROOT.'media');
+    *   foreach($files as $file) {
+    *     Tools_Media::beautify($file, 'min');
+    *   }
+    * </code>
+    * @param $file string file name
+    * @param $prefix string prefix to skip file (minimized not need to format)
+    * @return void
+    */
+    protected function beautify($file, $prefix = 'min')
+    {
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
+        if ($extension == 'css')
+          return $this->css_beautify($file, $prefix);
+        if ($extension == 'js')
+          return $this->js_beautify($file, $prefix);
+    }
+
+    /**
+    * beautify media files css(files with .min.js will be skiped)
+    *
+    * <code>
+    *  $files = Dir::files(DOCROOT.'media', 'css');
+    *   foreach($files as $file) {
+    *     Tools_Media::css_beautify($file, 'min');
+    *   }
+    * </code>
+    * @param $file string file name
+    * @param $prefix string prefix to skip file (minimized not need to format)
+    * @return void
+    */
+    protected function css_beautify($file, $prefix = 'min')
     {
         if ($prefix && preg_match('/\.'.$prefix.'\.css/', $file))
             return;
         $this->eightpack_exec('cssbeautify', $file);
     }
 
-    protected function js_beautify($file, $prefix = NULL)
+    /**
+    * beautify media files js(files with .min.js will be skiped)
+    *
+    * <code>
+    *  $files = Dir::files(DOCROOT.'media', 'js');
+    *   foreach($files as $file) {
+    *     Tools_Media::js_beautify($file, 'min');
+    *   }
+    * </code>
+    * @param $file string file name
+    * @param $prefix string prefix to skip file (minimized not need to format)
+    * @return void
+    */
+    protected function js_beautify($file, $prefix = 'min')
     {
         if ($prefix && preg_match('/\.'.$prefix.'\.js/', $file))
             return;
         $this->eightpack_exec('jsbeautify', $file);
     }
+
     public static function  check()
     {
     }
